349e1f9829231f1f8a6b3b949d240a75715755eb
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-05-20 22:30:13 -0300

    Inserido o método nextInt() nos parâmetros Um e Dois

--- src/Contador.java
+++ src/Contador.java
@@ -6,9 +6,9 @@ public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
+		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+		int parametroDois = terminal.nextInt();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
