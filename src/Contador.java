2bbc58b229aca83166b62d33acf5e7f3867cf111
rafaelsousadevbr <rafael@sousalima.dev.br>
2024-07-16 14:41:51 -0300

    Inserido um try para encerrar o objeto 'terminal' da classe Scanner

--- src/Contador.java
+++ src/Contador.java
@@ -5,34 +5,37 @@ public class Contador {
 
     public static void main(String[] args) {
 
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Para encerrar o programa digite 0 no campo do Primeiro parâmetro");
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        
-        if (parametroUm == 0) {
-            System.out.println("Programa finalizado com sucesso!");
-            System.exit(0);
-        }
-        do {
-            System.out.println("Digite o segundo parâmetro");
-            int parametroDois = terminal.nextInt();
-
-            try {
-                //chamando o método contendo a lógica de contagem
-                contar(parametroUm, parametroDois);
-
-            } catch (ParametrosInvalidosException exception) {
-                //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-            }
-
+        try (Scanner terminal = new Scanner(System.in)) {
             System.out.println("Para encerrar o programa digite 0 no campo do Primeiro parâmetro");
             System.out.println("Digite o primeiro parâmetro");
-            parametroUm = terminal.nextInt();
-        } while (parametroUm != 0);
-
-        System.out.println("Programa finalizado com sucesso!");
+            int parametroUm = terminal.nextInt();
+            
+            if (parametroUm == 0) {
+                System.out.println("Programa finalizado com sucesso!");
+                System.exit(0);
+            }
+            do {
+                System.out.println("Digite o segundo parâmetro");
+                int parametroDois = terminal.nextInt();
+                
+                try {
+                    //chamando o método contendo a lógica de contagem
+                    contar(parametroUm, parametroDois);
+                    
+                } catch (ParametrosInvalidosException exception) {
+                    //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+                    System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+                }
+                
+                System.out.println("Para encerrar o programa digite 0 no campo do Primeiro parâmetro");
+                System.out.println("Digite o primeiro parâmetro");
+                parametroUm = terminal.nextInt();
+                
+                
+            } while (parametroUm != 0);
+            
+            System.out.println("Programa finalizado com sucesso!");
+        }
 
     }
 
