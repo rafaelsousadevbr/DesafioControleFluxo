e94743976e64a9db9195629524bddf51d715f724
Rafael Sousa DEV <rafael@sousalima.dev.br>
2024-07-16 10:17:59 -0300

    Criado a mensagem de quando for lançada a exceção

--- src/Contador.java
+++ src/Contador.java
@@ -14,8 +14,9 @@ public class Contador {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (? exception) {
+		}catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro")
 		}
 		
 	}
